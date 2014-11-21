d7ca740fc326edd37c437e344513a77c12a4126c
TestSubject06 <zzzaaccckk@yahoo.com>
2014-11-21 18:30:55 -0500

    Combined the Gym and the Field together
    This combination is a part of FinalScene.Unity
    Please open it and look at how it works, but don't modify the actual
    scene file, or it'll make it harder to put it all together later.

---
+++ Assets/Scripts/DisableTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableTarget : MonoBehaviour {
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other){
+		target.SetActive(false);
+	}
+}
--- Assets/Scripts/SwapShaders.cs
+++ Assets/Scripts/SwapShaders.cs
@@ -5,6 +5,8 @@ public class SwapShaders : MonoBehaviour {
 
 	public Shader showThroughDoorway;
 	public Shader cullThroughDoorway;
+	public Shader portal;
+	public Shader portalZ;
 	public GameObject Zone1;
 	public GameObject Zone2;
 
@@ -16,6 +18,12 @@ public class SwapShaders : MonoBehaviour {
 			}else if(a.renderer != null && a.renderer.material.shader == showThroughDoorway){
 				a.renderer.material.shader = cullThroughDoorway;
 			}
+
+			if(a.renderer != null && a.renderer.material.shader == portal){
+				a.renderer.material.shader = portalZ;
+			}else if(a.renderer != null && a.renderer.material.shader == portalZ){
+				a.renderer.material.shader = portal;
+			}
 		}
 		allChildren = Zone2.GetComponentsInChildren<Transform>();
 		foreach(Transform a in allChildren){
@@ -23,6 +31,12 @@ public class SwapShaders : MonoBehaviour {
 				a.renderer.material.shader = cullThroughDoorway;
 			}else if(a.renderer != null && a.renderer.material.shader == cullThroughDoorway){
 				a.renderer.material.shader = showThroughDoorway;
+			}
+
+			if(a.renderer != null && a.renderer.material.shader == portal){
+				a.renderer.material.shader = portalZ;
+			}else if(a.renderer != null && a.renderer.material.shader == portalZ){
+				a.renderer.material.shader = portal;
 			}
 		}
 		Debug.Log("Highest Level Reached");
